d0bffcc27c957fb64008b199bd10f2486c65b098
ozodbekjumaev <ozodjon6@gmail.com>
2023-10-27 12:15:51 +0500

    fix some picture zone and smooth

---
+++ src/UIComponent/PlayButton.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg data-v-53c19b00="" width="203" height="203" viewBox="0 0 203 203" fill="none"
+       xmlns="http://www.w3.org/2000/svg">
+    <g data-v-53c19b00="" filter="url(#filter0_d_215_2459)">
+      <path data-v-53c19b00=""
+            d="M121.434 101.692C122.256 100.642 122.667 99.331 122.667 98.0253C122.667 96.5689 122.204 95.2126 121.331 94.108C121.254 94.0343 121.097 93.8674 120.894 93.6504C120.555 93.2891 120.085 92.7891 119.636 92.3498C115.423 87.9302 104.429 80.6983 98.6753 78.4885C97.8021 78.1369 95.5928 77.3886 94.4112 77.3334C93.2813 77.3334 92.2025 77.5895 91.175 78.0867C89.8906 78.7898 88.8632 79.8997 88.2981 81.2005C88.0376 81.8552 87.6694 83.4584 87.4861 84.3118C87.4069 84.6805 87.336 85.0526 87.2736 85.4246C86.7721 88.4156 86.5 92.9778 86.5 97.9747C86.5 102.733 86.7713 107.094 87.1866 110.108C87.2382 110.482 87.3222 110.886 87.3983 111.257C87.6078 112.276 88.0412 114.386 88.5035 115.257C89.6338 117.366 91.8429 118.667 94.2061 118.667H94.4112C94.6836 118.659 95.009 118.611 95.36 118.538C97.753 118.04 100.677 116.666 102.835 115.519C108.527 112.494 116.202 107.295 119.687 103.6L119.996 103.298C120.56 102.746 121.279 101.897 121.434 101.692Z"
+            fill="url(#paint0_linear_215_2459)"></path>
+      <path data-v-53c19b00="" fill-rule="evenodd" clip-rule="evenodd"
+            d="M158.918 97.4918C158.918 129.207 133.207 154.918 101.492 154.918C69.7762 154.918 44.0656 129.207 44.0656 97.4918C44.0656 65.7762 69.7762 40.0656 101.492 40.0656C133.207 40.0656 158.918 65.7762 158.918 97.4918ZM153.667 98.0248C153.667 126.487 130.477 149.667 102 149.667C73.5235 149.667 50.3333 126.487 50.3333 98.0248C50.3333 69.5123 73.5235 46.3333 102 46.3333C130.477 46.3333 153.667 69.5123 153.667 98.0248Z"
+            fill="url(#paint1_linear_215_2459)"></path>
+    </g>
+    <defs data-v-53c19b00="">
+      <filter data-v-53c19b00="" id="filter0_d_215_2459" x="0.0655732" y="0.0655746" width="202.853"
+              height="202.853" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood data-v-53c19b00="" flood-opacity="0" result="BackgroundImageFix"></feFlood>
+        <feColorMatrix data-v-53c19b00="" in="SourceAlpha" type="matrix"
+                       values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                       result="hardAlpha"></feColorMatrix>
+        <feOffset data-v-53c19b00="" dy="4"></feOffset>
+        <feGaussianBlur data-v-53c19b00="" stdDeviation="22"></feGaussianBlur>
+        <feComposite data-v-53c19b00="" in2="hardAlpha" operator="out"></feComposite>
+        <feColorMatrix data-v-53c19b00="" type="matrix"
+                       values="0 0 0 0 1 0 0 0 0 0.784314 0 0 0 0 0.462745 0 0 0 0.24 0"></feColorMatrix>
+        <feBlend data-v-53c19b00="" mode="normal" in2="BackgroundImageFix"
+                 result="effect1_dropShadow_215_2459"></feBlend>
+        <feBlend data-v-53c19b00="" mode="normal" in="SourceGraphic" in2="effect1_dropShadow_215_2459"
+                 result="shape"></feBlend>
+      </filter>
+      <linearGradient data-v-53c19b00="" id="paint0_linear_215_2459" x1="44.0656" y1="154.918"
+                      x2="164.637" y2="148.562" gradientUnits="userSpaceOnUse">
+        <stop data-v-53c19b00="" stop-color="#A5834D"></stop>
+        <stop data-v-53c19b00="" offset="0.392137" stop-color="#FFC876"></stop>
+        <stop data-v-53c19b00="" offset="1" stop-color="#FFC876"></stop>
+      </linearGradient>
+      <linearGradient data-v-53c19b00="" id="paint1_linear_215_2459" x1="44.0656" y1="154.918"
+                      x2="164.637" y2="148.562" gradientUnits="userSpaceOnUse">
+        <stop data-v-53c19b00="" stop-color="#A5834D"></stop>
+        <stop data-v-53c19b00="" offset="0.392137" stop-color="#FFC876"></stop>
+        <stop data-v-53c19b00="" offset="1" stop-color="#FFC876"></stop>
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/AnimatePage.vue
+++ src/components/AnimatePage.vue
@@ -3,27 +3,44 @@
     <div class="wrapper min-h-screen">
       <div class="min-h-screen relative">
         <div class="container-animation min-h-screen w-full h-full absolute inset-0">
-          <div class="relative">
-            <div class="absolute w-full h-full top-[10%] right-[0]">
+          <div class="relative w-full">
+            <div class="absolute w-full h-full top-[0] right-[0]">
               <div class="container h-full">
-                <img src="../assets/5.png"  alt="" class="absolute img cursor-pointer scale-[1.7] top-[64px] left-[-120px] z-[999]" id="image-1" />
-                <img src="../assets/2.png"  alt="" class="absolute img cursor-pointer scale-[1.7] top-[-120px] left-[40px] z-[999]" id="image-2" />
-                <img src="../assets/3.png"  alt="" class="absolute img cursor-pointer scale-[1.7] top-[-110px] right-[40px] z-[999]" id="image-3" />
-                <img src="../assets/4.png"  alt="" class="absolute img cursor-pointer scale-[1.7] top-[124px] right-[-140px] z-[999]" id="image-4" />
-                <img src="../assets/1.png"  alt="" class="absolute img cursor-pointer scale-[1.7] bottom-[90px] right-[-130px] z-[999]" id="image-5" />
-                <img src="../assets/6.png"  alt="" class="absolute img cursor-pointer scale-[2.5] bottom-[0px] right-[80px] z-[999]" id="image-6" />
-                <img src="../assets/7.png"  alt="" class="absolute img cursor-pointer scale-[2.5] bottom-[0px] right-[40%] z-[999]" id="image-7" />
-                <img src="../assets/8.png"  alt="" class="absolute img cursor-pointer scale-[2.8] bottom-[-23px] left-[190px] z-[999]" id="image-8" />
-                <img src="../assets/9.png"  alt="" class="absolute img cursor-pointer scale-[2.7] bottom-[210px] left-[-180px] z-[999]" id="image-9" />
+                <img src="../assets/5.png" alt=""
+                     class="absolute img cursor-pointer scale-[1.7] top-[104px] left-[-50px] z-[999]" id="image-1"/>
+                <img src="../assets/2.png" alt=""
+                     class="absolute img cursor-pointer scale-[1.7] top-[-90px] left-[120px] z-[999]" id="image-2"/>
+                <img src="../assets/3.png" alt=""
+                     class="absolute img cursor-pointer scale-[2.7] top-[-110px] right-[160px] z-[999]" id="image-3"/>
+                <img src="../assets/4.png" alt=""
+                     class="absolute img cursor-pointer scale-[2.7] top-[190px] right-[-160px] z-[999]" id="image-4"/>
+                <img src="../assets/1.png" alt=""
+                     class="absolute img cursor-pointer scale-[2.7] bottom-[160px] right-[-140px] z-[999]"
+                     id="image-5"/>
+                <img src="../assets/6.png" alt=""
+                     class="absolute img cursor-pointer scale-[2.7] bottom-[50px] right-[180px] z-[999]" id="image-6"/>
+                <img src="../assets/7.png" alt=""
+                     class="absolute img cursor-pointer scale-[3] bottom-[-180px] right-[40%] z-[999]" id="image-7"/>
+                <img src="../assets/8.png" alt=""
+                     class="absolute img cursor-pointer scale-[2.8] bottom-[-40px] left-[190px] z-[999]" id="image-8"/>
+                <img src="../assets/9.png" alt=""
+                     class="absolute img cursor-pointer scale-[3] top-[50%] left-[-180px] z-[999]" id="image-9"/>
               </div>
             </div>
-            <div class="relative">
-              <div class="overlay absolute z-[999] top-0 right-0 w-full h-full flex-col flex items-center justify-center"
-                   id="image-uzb"
+            <div class="relative overflow-hidden">
+              <div
+                  class="overlay absolute z-[999] top-0 right-0 w-full h-full flex-col flex items-center justify-center"
+                  id="image-uzb"
               >
                 <img src="../assets/uzbekistans.png" alt="">
               </div>
-              <video src="../assets/uzbekistans.mp4" class="w-full min-h-screen object-cover" autoplay playsinline muted loop />
+              <video src="../assets/uzbekistans.mp4" class="w-full min-h-screen object-cover" autoplay playsinline muted
+                     loop/>
+              <div class="absolute top-0 right-0 w-full h-full bg-[#00000040] flex items-end justify-center">
+                <a href="https://uzbekistans.club/projects/ozbekistonlik" class="player mb-[260px]">
+                  <PlayButton/>
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -36,6 +53,7 @@
 import {gsap} from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger"
 import {onMounted} from "vue";
+import PlayButton from "../UIComponent/PlayButton.vue"
 
 
 gsap.registerPlugin(ScrollTrigger)
@@ -44,18 +62,19 @@ function animatePage() {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: '.wrapper',
-      start: 'top top',
-      end: 'bottom center',
+      start: 'top',
+      end: 'bottom -300px',
       markers: false,
-      scrub: 4,
+      scrub: 5,
       pin: true,
       pinSpacing: true,
       toggleActions: 'play none none reverse'
     }
   })
-  
+
   tl.to('.container-animation', {
-    scale: 0.3
+    scale: 0.4,
+    x: '5%'
   })
 
   tl.from('#image-1', {
@@ -121,21 +140,20 @@ function animatePage() {
     opacity: 0
   }, "-=0.5")
 
-
-  tl.to('.container-animation', {
-    x: 300
-  })
-
   tl.from('#image-uzb', {
     x: -2000,
     opacity: 0,
+  }, "-=0.5")
+
+  tl.to('.container-animation', {
+    scale: 0.3,
+    x: '20%'
   })
 
   tl.to('#image-uzb', {
     x: 0,
     opacity: 1
   })
-
 
 
 }
